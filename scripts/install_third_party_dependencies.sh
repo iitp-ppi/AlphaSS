737d617f3481fd39865561dda855dd2ce3a0992f
sehooni <74sehoon@gmail.com>
2025-01-13 20:38:59 +0900

    revise the install

--- scripts/install_third_party_dependencies.sh
+++ scripts/install_third_party_dependencies.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 source scripts/vars.sh
 
+conda env create --name=${ENV_NAME} -f environment.yml
+source activate ${ENV_NAME}
+
+echo "Attempting to install FlashAttention"
+git clone https://github.com/HazyResearch/flash-attention
+CUR_DIR=$PWD
+cd flash-attention
+git checkout 5b838a8bef
+python3 setup.py install
+cd $CUR_DIR
 
 # Install DeepMind's OpenMM patch
 OPENFOLD_DIR=$PWD
-pushd /home/bis/anaconda3/envs/PSH_alphalink/lib/python3.9/site-packages \
+# for line 11, you have to insert your conda path!
+pushd /home/bis/anaconda3/envs/${ENV_NAME}/lib/python3.9/site-packages \
     && patch -p0 < $OPENFOLD_DIR/lib/openmm.patch \
     && popd
 
@@ -16,8 +27,15 @@ wget -q -P openfold/resources \
 mkdir -p tests/test_data/alphafold/common
 ln -rs openfold/resources/stereo_chemical_props.txt tests/test_data/alphafold/common
 
-# Download pretrained openfold weights
-scripts/download_alphafold_params.sh openfold/resources
+echo "Downloading OpenFold parameters..."
+bash scripts/download_openfold_params.sh openfold/resources
+
+echo "Downloading AlphaFold parameters..."
+bash scripts/download_alphafold_params.sh openfold/resources
 
 # Decompress test data
 gunzip tests/test_data/sample_feats.pickle.gz
+
+
+cd $CUR_DIR
+CC=g++ python setup.py install
